55309e32087b2057e3a3103c12936b253aefda9e
Sagar Mainali <sagarmainali78@gmail.com>
2023-06-01 08:32:12 +0545

    removed unused dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React, { useRef } from 'react'
 import './style.css'
-// import Carousel from "react-multi-carousel";
-// import "react-multi-carousel/lib/styles.css";
 
 const App = () => {
 
@@ -13,28 +11,6 @@ const App = () => {
   for (index = 0; index < 15; index++) {
     carouselItems.push(`Item_${index + 1}`)
   }
-  // const responsive = {
-  //   superLargeDesktop: {
-  //     // the naming can be any, depends on you.
-  //     breakpoint: { max: 4000, min: 3000 },
-  //     items: 5
-  //   },
-  //   desktop: {
-  //     breakpoint: { max: 3000, min: 1024 },
-  //     items: 4
-  //   },
-  //   tablet: {
-  //     breakpoint: { max: 1024, min: 464 },
-  //     items: 2
-  //   },
-  //   mobile: {
-  //     breakpoint: { max: 464, min: 0 },
-  //     items: 1
-  //   }
-  // };
-  // responsive={responsive}
-  // swipeable={true}
-  // containerClass='carousel-container'
 
   function slider(direction) {
 
